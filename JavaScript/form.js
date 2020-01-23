c4e16015e04cf24a31d628c74f929fb09527d3a0
eiribarev <eiribarev@codingburgas.bg>
2020-01-23 16:00:06 +0200

    Added a dropdown to the contact form

--- JavaScript/form.js
+++ JavaScript/form.js
@@ -1,8 +1,10 @@
 function formsubmit() {
 	var name = document.getElementById("contactForm").elements["name"].value;
 	var subject = document.getElementById("contactForm").elements["subject"].value;
+	var type = document.getElementById("contactForm").elements["type"].value;
 
-	var mailtohref = "mailto:blue.energy@codingburgas.bg?subject=" + name + ": " + subject;
+	var mailtohref = "mailto:blue.energy@codingburgas.bg?";
+	mailtohref += "subject=" + type + ": " + subject;
 
 	document.getElementById("mailLink").href = mailtohref;
 
