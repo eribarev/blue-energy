e3a157e05bbadf9ba288d9cd9d3c21240ba19625
eiribarev <eiribarev@codingburgas.bg>
2020-01-26 10:59:39 +0200

    Added a textarea to the contact form

--- JavaScript/form.js
+++ JavaScript/form.js
@@ -2,11 +2,21 @@ function formsubmit() {
 	var name = document.getElementById("contactForm").elements["name"].value;
 	var subject = document.getElementById("contactForm").elements["subject"].value;
 	var type = document.getElementById("contactForm").elements["type"].value;
+	var bodytext = document.getElementById("contactForm").elements["bodytext"].value;
 
 	var mailtohref = "mailto:blue.energy@codingburgas.bg?";
 	mailtohref += "subject=" + type + ": " + subject;
+	mailtohref += "&body=" + escape(bodytext);
 
 	document.getElementById("mailLink").href = mailtohref;
 
 	document.getElementById("mailLink").click();
+}
+
+function textareaPlaceholderUpdate() {
+	var type = document.getElementById("contactForm").elements["type"].value;
+
+	type = type[0].toLowerCase() + type.substring(1);
+
+	document.getElementById("contactForm").elements["bodytext"].placeholder = "Please enter your " + type + " here";
 }
