35244283e307aea96090e651670182c36dab4acc
eiribarev <eiribarev@codingburgas.bg>
2020-01-26 16:53:47 +0200

    Added comments to form.js

--- JavaScript/form.js
+++ JavaScript/form.js
@@ -1,9 +1,11 @@
 function formsubmit() {
+	//get all form data
 	var name = document.getElementById("contactForm").elements["name"].value;
 	var subject = document.getElementById("contactForm").elements["subject"].value;
 	var type = document.getElementById("contactForm").elements["type"].value;
 	var bodytext = document.getElementById("contactForm").elements["bodytext"].value;
 
+	//generate mailto href
 	var mailtohref = "mailto:blue.energy@codingburgas.bg?";
 	mailtohref += "subject=" + type + ": " + subject;
 	mailtohref += "&body=" + escape(bodytext);
@@ -14,9 +16,12 @@ function formsubmit() {
 }
 
 function textareaPlaceholderUpdate() {
+	//get select value
 	var type = document.getElementById("contactForm").elements["type"].value;
 
+	//make first letter lowercase
 	type = type[0].toLowerCase() + type.substring(1);
 
+	//assign new placeholder for the textarea
 	document.getElementById("contactForm").elements["bodytext"].placeholder = "Please enter your " + type + " here";
 }
